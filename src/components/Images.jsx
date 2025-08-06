07ae71d5cb67a5d8c185d2217d3885b736aee5c8
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2025-08-06 10:56:50 -0700

    moved imaes to assets folder and updated code in Images.jsx

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,13 +1,20 @@
 import './Images.css';
+import burningpoint from "../assets/burningpoint.png";
+import echoesofthewild from "../assets/echoesofthewild.png";
+import midnightinflorence from "../assets/midnightinflorence.png";
+import sunshinedays from "../assets/sunshinedays.png";
+import thelastletter from "../assets/thelastletter.png";
+import whispersinthedark from "../assets/whispersinthedark.png";
+
 export default function Images(){
     return (
         <div className="img-card">
-            <img src="../public/burningpoint.png" alt="Burning Point" />
-            <img src="../public/echoesofthewild.png" alt="Echoes of the Wild" />
-            <img src="../public/midnightinflorence.png" alt="Midnight in Florence" />
-            <img src="../public/sunshinedays.png" alt="Sunshine Days" />
-            <img src="../public/thelastletter.png" alt="The Last Letter" />
-            <img src="../public/whispersinthedark.png" alt="Whispers in the Dark" />
+            <img src={burningpoint} alt="Burning Point" />
+            <img src={echoesofthewild} alt="Echoes of the Wild" />
+            <img src={midnightinflorence} alt="Midnight in Florence" />
+            <img src={sunshinedays} alt="Sunshine Days" />
+            <img src={thelastletter} alt="The Last Letter" />
+            <img src={whispersinthedark} alt="Whispers in the Dark" />
         </div>
     );
 }
