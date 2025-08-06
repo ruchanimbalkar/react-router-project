99932ab3ccfe71faefa62e4f640e5a1939ddfcc8
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2025-08-06 09:08:15 -0700

    my first react router project'

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+
+    </>
+  )
+}
+
+export default App
